6396a602b1a6508e2d56cee77aee4d36a26c31bd
Petrelis-Efstratios <petrelis-stratos@outlook.com.gr>
2023-01-08 18:33:45 +0200

    Created the boyd of the page and some of the function of the calculator

---
+++ calculator.js
@@ -0,0 +1,17 @@
+const operations = document.querySelector(".operations");
+const operation = [];
+let currentNumber = ""
+let zero = () => currentNumber += 1;
+let one = () => currentNumber += 1;
+let two = () => currentNumber += 1;
+let three = () => currentNumber += 1;
+let four = () => currentNumber += 1;
+let five = () => currentNumber += 1;
+let six = () => currentNumber += 1;
+let seven = () => currentNumber += 1;
+let heigth = () => currentNumber += 1;
+let nine = () => currentNumber += 1;
+let plus = () => ;
+let minus = () => ;
+let by = () => ;
+let devides = () => ;
