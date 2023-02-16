e7d481dde5f57931ea19532f1207c866ccba33f6
Petrelis-Efstratios <petrelis-stratos@outlook.com.gr>
2023-02-16 21:54:46 +0200

    Fixed the bug that returned 0 if there was an * or / at the end Cleaned the code

--- calculator.js
+++ calculator.js
@@ -4,38 +4,38 @@ let currentNumber = "";
 let arr = ["0"];
 let result = 0;
 let EQUALS = false;
-let zero = () => {something("0"); changeCurrent("number");};
-let one = () => {something("1"); changeCurrent("number");};
-let two = () => {something("2"); changeCurrent("number");};
-let three = () => {something("3"); changeCurrent("number");};
-let four = () => {something("4"); changeCurrent("number");};
-let five = () => {something("5"); changeCurrent("number");};
-let six = () => {something("6"); changeCurrent("number");};
-let seven = () => {something("7"); changeCurrent("number");};
-let eigth = () => {something("8"); changeCurrent("number");};
-let nine = () => {something("9"); changeCurrent("number");};
+let zero = () => {changeArray("0"); changeCurrent("number");};
+let one = () => {changeArray("1"); changeCurrent("number");};
+let two = () => {changeArray("2"); changeCurrent("number");};
+let three = () => {changeArray("3"); changeCurrent("number");};
+let four = () => {changeArray("4"); changeCurrent("number");};
+let five = () => {changeArray("5"); changeCurrent("number");};
+let six = () => {changeArray("6"); changeCurrent("number");};
+let seven = () => {changeArray("7"); changeCurrent("number");};
+let eigth = () => {changeArray("8"); changeCurrent("number");};
+let nine = () => {changeArray("9"); changeCurrent("number");};
 let point = () => {
     if(!current.textContent.includes(".") || EQUALS) {
-        something(".");
+        changeArray(".");
         changeCurrent(".");
     }
 }
 let plus = () => {
     currentNumber = "";
     changeCurrent("");
-    something("+");
+    changeArray("+");
 };
 let minus = () => {
     changeCurrent("");
-    something("−");
+    changeArray("−");
 };
 let by = () => {
     changeCurrent("");
-    something("×");
+    changeArray("×");
 };
 let devides = () => {
     changeCurrent("");
-    something("÷")
+    changeArray("÷")
 };
 let AC = () => {
     arr = ["0"];
@@ -44,7 +44,7 @@ let AC = () => {
     changeCurrent("AC");
     past.textContent = "";
 }
-function backspace() {
+let backspace = () => {
     if(!EQUALS) {
         if(current.textContent.length === 1 && !past.textContent) {
             past.textContent = "";
@@ -69,25 +69,27 @@ function backspace() {
     }
 }
 let equals = () => {
-    if(arr.length < 2) {
+    if(arr.length < 2 && Number(arr[0] != arr[0])) {
+        result = arr[0];
+    } else if(arr.length < 2 && Number(arr[0] == arr[0]))  {
         result = String(Math.round(arr[0]));
     } else {
         past.textContent = arr.join(" ");
         result = arr[arr.length - 2];
         if(past.textContent.substring(0, past.textContent.length - 1).includes("×") || past.textContent.substring(0, past.textContent.length - 1).includes("÷")) {
-        for(let i = 0; i < arr.length; i++) {
-            if (arr[i] === "×") {
-                result = String(parseFloat(parseFloat(String(Number(arr[i - 1]) * Number(arr[i + 1]))).toFixed(8)));
-                arr.splice(i - 1, 3, result);
-                i--;
-            } else if(arr[i] === "÷") {
-                result = String(parseFloat(parseFloat(String(Number(arr[i - 1]) / Number(arr[i + 1]))).toFixed(8)));
-                arr.splice(i - 1, 3, result);
-                i--;
+                for(let i = 0; i < arr.length; i++) {
+                    if(arr[i] === "×" && i !== arr.length - 1) {
+                        result = String(parseFloat(parseFloat(String(Number(arr[i - 1]) * Number(arr[i + 1]))).toFixed(8)));
+                        arr.splice(i - 1, 3, result);
+                        i--;
+                    } else if(arr[i] === "÷" && i !== arr.length - 1) {
+                        result = String(parseFloat(parseFloat(String(Number(arr[i - 1]) / Number(arr[i + 1]))).toFixed(8)));
+                        arr.splice(i - 1, 3, result);
+                        i--;
+                    }
+            }
         }
-    }
-}
-    for(let i = 0; i < arr.length; i++) {
+        for(let i = 0; i < arr.length; i++) {
             if(i === arr.length - 1) {
                 arr = [result];
             } else if(arr[i] === "+") {
@@ -100,58 +102,56 @@ let equals = () => {
         }
     }
     if(result === "Infinity" || result === "NaN") {
-         current.textContent = "You can't devide by 0";           
+        current.textContent = "You can't devide by 0";           
     } else {
         current.textContent = "= " + result;
     }
     EQUALS = true;
 }
-function changeCurrent(num) {
-    if(num === "number" || num === ".") {
+function changeCurrent(type) {
+    if(type === "number" || type === ".") {
         current.textContent = arr[arr.length - 1];
-    } else if (num === "") {
+    } else if(type === "") {
         current.textContent = "";
-    } else if(num === "backspace") {
-        if(Number(arr[arr.length - 1]) == arr[arr.length - 1]) {
-            current.textContent = arr[arr.length - 1];
-        }
-    } else if(num === "AC") {
+    } else if(type === "backspace" && Number(arr[arr.length - 1]) == arr[arr.length - 1]) {
+        current.textContent = arr[arr.length - 1];
+    } else if(type === "AC") {
         current.textContent = "0";
     }
 }
-function something(curNum) {
-    if(Number(curNum) == curNum || curNum === ".") {
+function changeArray(input) {
+    if(Number(input) == input || input === ".") {
         if(arr[arr.length - 1] === "+" || arr[arr.length - 1] === "−" || arr[arr.length - 1] === "×" || arr[arr.length - 1] === "÷") {
-            if(curNum === ".") {
-                arr.push(`0${curNum}`);
+            if(input === ".") {
+                arr.push(`0${input}`);
             } else {
-                arr.push(curNum);
+                arr.push(input);
             }
-        } else if(arr[arr.length - 1] === "0" && curNum === ".") {
+        } else if(arr[arr.length - 1] === "0" && input === ".") {
             arr[arr.length - 1] = "0.";
         } else if(arr[arr.length - 1] === "0") {
-            arr.splice(arr.length - 1, 1, curNum)
+            arr.splice(arr.length - 1, 1, input)
             current.textContent = current.textContent.slice(1);
-        } else if(EQUALS && curNum === ".") {
+        } else if(EQUALS && input === ".") {
             arr = ["0."];
             EQUALS = false;
             past.textContent = "";
         } else if(EQUALS) {
-            arr = [curNum];
+            arr = [input];
             past.textContent = "";
         } else {
-            arr[arr.length - 1] += curNum;
+            arr[arr.length - 1] += input;
         }
     } else {
         if(arr[arr.length - 1] === "+" || arr[arr.length - 1] === "−" || arr[arr.length - 1] === "×" || arr[arr.length - 1] === "÷") {
-            arr.splice(arr.length - 1, 1, curNum);
+            arr.splice(arr.length - 1, 1, input);
         } else if(arr[0] === "Infinity" || arr[0] === ["NaN"]) {
-            arr = ["0", curNum];
+            arr = ["0", input];
         } else if(Math.round(arr[arr.length - 1]) == arr[arr.length - 1]) {
             arr.splice(arr.length - 1, 1, String(Math.round(arr[arr.length - 1])));
-            arr.push(curNum);
+            arr.push(input);
         } else {
-            arr.push(curNum);
+            arr.push(input);
         }
     past.textContent = arr.join(" ");
     past.scrollTop = past.scrollHeight - past.clientHeight;
@@ -211,16 +211,20 @@ window.onkeydown = function(e) {
             break;
         case "Enter":
             equals();
-            buttonKey = "="
+            buttonKey = "=";
             break;
         case "Delete":
             AC();
+            break;
+        case "Escape":
+            AC();
+            buttonKey = "Delete";
             break;
         case "Backspace":
             backspace();
             break;
     }
-    if(buttonKey === "0" || buttonKey === "1" || buttonKey === "2" || buttonKey === "3" || buttonKey === "4" || buttonKey === "5" ||  buttonKey === "6" ||  buttonKey === "7" || buttonKey === "8" ||  buttonKey === "9" ||  buttonKey === "+" || buttonKey === "-" || buttonKey === "*" || buttonKey === "/" || buttonKey === "=" || buttonKey === "." || buttonKey === "Delete" || buttonKey === "Backspace") {
+    if(Number(buttonKey) == buttonKey ||  buttonKey === "+" || buttonKey === "-" || buttonKey === "*" || buttonKey === "/" || buttonKey === "=" || buttonKey === "." || buttonKey === "Delete" || buttonKey === "Backspace") {
         const button = document.querySelector(`[key="${buttonKey}"]`);
         if(button.classList.value === "AC" || button.classList.value === "backspace") {
             button.style.cssText = "bottom: 3px; border: 4px solid #ff1300";
